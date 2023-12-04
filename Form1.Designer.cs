c753f42e93aa5dd92020fb73e7cdd77cd3942d4a
Dmitrii Stepanov <dstepanov200498@gmail.com>
2023-12-05 00:22:26 +0400

    .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System.IO;
+using System;
+
 namespace WindowsFormsApp1
 {
     partial class Form1
@@ -7,6 +9,25 @@ namespace WindowsFormsApp1
         /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
+
+        private class BackUp
+        {
+            private string sourse_of_backup;
+            private string destination_of_backup;
+            private string finc_of_backup;
+            private string sinc_of_backup;
+
+            private BackUp()
+            {
+                sourse_of_backup = null;
+                destination_of_backup = null;
+                finc_of_backup = null;
+                sinc_of_backup = null;
+            }
+
+
+
+        }
 
         /// <summary>
         /// Освободить все используемые ресурсы.
@@ -30,103 +51,116 @@ namespace WindowsFormsApp1
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
             this.button2 = new System.Windows.Forms.Button();
-            this.menuStrip1.SuspendLayout();
+            this.button3 = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 56);
+            this.button1.Location = new System.Drawing.Point(12, 28);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(158, 23);
+            this.button1.Size = new System.Drawing.Size(491, 32);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Выбрать директорию";
+            this.button1.Text = "Выбрать директорию для РК";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // menuStrip1
+            // toolStrip1
             // 
-            this.menuStrip1.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.файлToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripLabel2,
+            this.toolStripLabel3});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(980, 25);
+            this.toolStrip1.TabIndex = 8;
+            this.toolStrip1.Text = "toolStrip1";
             // 
-            // файлToolStripMenuItem
+            // toolStripLabel1
             // 
-            this.файлToolStripMenuItem.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.сохранитьToolStripMenuItem,
-            this.открытьToolStripMenuItem});
-            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
-            this.файлToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
-            this.файлToolStripMenuItem.Text = "файл";
-            this.файлToolStripMenuItem.Click += new System.EventHandler(this.файлToolStripMenuItem_Click);
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(105, 22);
+            this.toolStripLabel1.Text = "Настройки РК";
+            this.toolStripLabel1.Click += new System.EventHandler(this.toolStripLabel1_Click);
             // 
-            // сохранитьToolStripMenuItem
+            // toolStripLabel2
             // 
-            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
-            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.сохранитьToolStripMenuItem.Text = "сохранить";
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(60, 22);
+            this.toolStripLabel2.Text = "Бэкапы";
             // 
-            // открытьToolStripMenuItem
+            // toolStripLabel3
             // 
-            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
-            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
-            this.открытьToolStripMenuItem.Text = "открыть";
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 28);
-            this.panel1.TabIndex = 2;
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(123, 22);
+            this.toolStripLabel3.Text = "Восстановление";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 85);
+            this.button2.Location = new System.Drawing.Point(12, 66);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(158, 23);
-            this.button2.TabIndex = 3;
+            this.button2.Size = new System.Drawing.Size(491, 32);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Смотреть бэкапы";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(491, 32);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Восстановление";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(980, 546);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.menuStrip1);
-            this.Controls.Add(this.panel1);
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Приложуха";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
+
+        }
+
+        private void CreateBackUp(string path_from_create_backup, string path_to_create_backup)
+        {
+
+            if (path_to_create_backup == null || path_from_create_backup == null)
+            {
+                // error try again
+            }
+
+
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,10 +19,39 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string path_from_create_backup = null;
+            string path_to_create_backup = null;
+
+            FolderBrowserDialog DirDialog = new FolderBrowserDialog(); // выбор папки
+            DirDialog.Description = "Выбор директории откуда";
+            DirDialog.SelectedPath = @"С:\";
+            if (DirDialog.ShowDialog() == DialogResult.OK)
+            {
+                path_from_create_backup = DirDialog.SelectedPath;
+            }
+
+            FolderBrowserDialog DirDialog1 = new FolderBrowserDialog(); // выбор папки
+            DirDialog1.Description = "Выбор директории куда";
+            DirDialog1.SelectedPath = @"С:\";
+            if (DirDialog1.ShowDialog() == DialogResult.OK)
+            {
+                path_to_create_backup = DirDialog1.SelectedPath;
+            }
+
+            CreateBackUp(path_from_create_backup, path_to_create_backup);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
 
         }
 
-        private void файлToolStripMenuItem_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
